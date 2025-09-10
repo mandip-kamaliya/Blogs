72149099b7a3489bb4612e18a0cc037da06b3ec6
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-10 11:45:23 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT || 3000 ; 
+
+app.get("/",(req,res)=>{
+    res.status(200).json("Api is running!!");
+})
+
+app.listen(port, ()=>{
+    console.log(`server is runnign on ${port} port`);
+})
---
+++ prisma/migrations/20250910061029_init_user_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('AUTHOR', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'AUTHOR',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
