2c0105f8d532d5df87b5d239ea493c2bd6c97623
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-11 21:14:44 +0530

    add auth route in index.js file

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
 import "dotenv/config";
 
-import AuthRoute
+import {AuthRoute} from "./routes/AuthRoute.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth",AuthRoute);
 
 const port = process.env.PORT || 3000 ; 
 
 app.get("/",(req,res)=>{
     res.status(200).json("Api is running!!");
 })
+
+
 
 app.listen(port, ()=>{
     console.log(`server is runnign on ${port} port`);
