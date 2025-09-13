be25818d504afa2ba4562804dc7431990bd39682
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-13 16:52:24 +0530

    added protected Route

--- routes/protectedRoute.js
+++ routes/protectedRoute.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-
+import authMiddleware from "../middleware/authmiddleware";
 const router = Router();
 
 router.get("/test",authMiddleware,(req,res)=>{
-
+     res.json({
+        message: 'You have accessed a protected route!',
+        user: req.user 
+    });
 })
+
+export default router;
 
 
 
