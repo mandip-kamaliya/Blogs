93c41f63d99fd1795ea9d285e4b927f8ae28831c
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-13 16:59:32 +0530

    updating index.js by adding postRoute

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import "dotenv/config";
 
 import AuthRoute from "./routes/AuthRoute.js";
+import postRoute from "./routes/postRoute.js"
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth",AuthRoute);
+
+app.use("/api/posts",postRoute);
 
 const port = process.env.PORT || 3000 ; 
 
