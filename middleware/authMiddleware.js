8b4063ef1d1fc0653bc26c22f8e39cceb141d030
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-13 16:47:44 +0530

    added auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req,res,next) =>{
+    const authheader = req.header.authorization;
+
+    if(!authheader || authheader.startsWith('Bearer')){
+        return res.status(401).json("header not available");
+    }
+
+    const token = authheader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    
+        req.user = decoded;
+        next();
+    } catch (error) {
+       return res.status(401).json("server error");
+    }
+}
