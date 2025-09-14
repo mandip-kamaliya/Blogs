b271982e42c4211d6016613d5c4642191a2c43f4
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-14 18:45:35 +0530

    added post route for posting blog

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -13,8 +13,27 @@ router.get("/test",authMiddleware,(req,res)=>{
     });
 });
 
-router.post("/post")
+router.post("/",authMiddleware,async (req,res)=>{
+    const {title,content} = req.body;
+    const authorId = req.user.userId;
 
+    if(!title || !content){
+        return res.status(400).json("Title and content are requird");
+    }
+
+    try {
+        const post = await prisma.post.create({
+            title,
+            content,
+            authorId
+        });
+        res.status(201).json("new post created");
+
+    } catch (error) {
+         res.status(500).json("server error");
+    }
+
+})
 export default router;
 
 
