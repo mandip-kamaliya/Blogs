68ca7759e2e09732c757c0481b51e9cc84e655fa
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-14 18:36:09 +0530

    initialize prisma client

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/authmiddleware";
 const router = Router();
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
 
 router.get("/test",authMiddleware,(req,res)=>{
      res.json({
         message: 'You have accessed a protected route!',
         user: req.user 
     });
-})
+});
+
+router.post("/post")
 
 export default router;
 
